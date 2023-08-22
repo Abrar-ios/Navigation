6cd2448009f4207fad83d78a62340f52dbd91ba3
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-23 01:01:35 +0500

    Create SegueNextViewController

---
+++ Navigation/SegueNextViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SegueNextViewController.swift
+//  Navigation
+//
+//  Created by HAQQQABD on 23/08/2023.
+//
+
+import UIKit
+
+class SegueNextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
