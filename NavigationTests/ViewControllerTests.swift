77fa26efda5b6f031ac20a3e655783f82859f4c9
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-24 20:29:39 +0500

    Add ViewControllerTests
    .Implement test_zero to confirm communication is working between test suit and code

---
+++ NavigationTests/ViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  ViewControllerTests.swift
+//  NavigationTests
+//
+//  Created by HAQQQABD on 24/08/2023.
+//
+
+import XCTest
+@testable import Navigation
+
+final class ViewControllerTests:XCTestCase {
+    func test_zero(){
+        XCTFail("There is no test implemented yer in ViewControllerTests yet")
+    }
+}
